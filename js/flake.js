e65549342ef421d61362ce6a589e43dc7b5ce4f7
Flo Süsstrunk <flo.suesstrunk@gmail.com>
2013-08-31 23:59:13 +0200

    adding styling osx

--- js/flake.js
+++ js/flake.js
@@ -1,4 +1,24 @@
 (function ($, window) {
   "use strict";
 
+  // flake plugin
+  var flake = {
+    init: function () {
+      console.log("init");
+      this.version = "10.2.r14";
+      window.oncontextmenu = function () {
+        flake.menu();
+        return false;
+      };
+    },
+    menu: function () {
+      console.log("menu");
+    },
+    remove: function () {
+      console.log("remove");
+    }
+  };
+
+  flake.init();
+
 }(jQuery, window));
