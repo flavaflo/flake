63e1a494055112bee34acbb435e8acd0d4c829f7
Flo Süsstrunk <flo.suesstrunk@gmail.com>
2013-09-01 00:15:36 +0200

    adding localization

--- js/flake.js
+++ js/flake.js
@@ -1,18 +1,47 @@
-(function ($, window) {
+(function ($, document) {
   "use strict";
 
   // flake plugin
   var flake = {
     init: function () {
-      console.log("init");
+      var self = this;
+      this.$flake = $('.flake');
       this.version = "10.2.r14";
-      window.oncontextmenu = function () {
+      this.lang = "de";
+      this.locales = {
+        en: {
+          zoomin: "Zoom In",
+          zoomout: "Zoom On",
+          showall: "Show all",
+          quality: "Quality",
+          quality_low: "Low",
+          quality_medium: "Medium",
+          quality_hight: "High",
+          play: "Play",
+          loop: "Loop"
+        },
+        de: {
+
+        }
+      };
+
+      document.oncontextmenu = function () {
         flake.menu();
         return false;
       };
+
+      $(document).on("click", function (e) {
+        e.preventDefault();
+        self.$flake.removeClass("is_active");
+      });
     },
+
+    localizedString: function (identifier) {
+      return this.locales[this.lang][identifier] || identifier;
+    },
+
     menu: function () {
-      console.log("menu");
+      this.$flake.toggleClass("is_active");
     },
     remove: function () {
       console.log("remove");
@@ -21,4 +50,4 @@
 
   flake.init();
 
-}(jQuery, window));
+}(jQuery, document));
